6902261f9e00b9b678c370a1ee4152c75035879f
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-25 13:54:05 +0900

    ok

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
+
 
 const app = express();
-
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+  }
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
